7157c1ae65b46a279db80f9a8d999d94a61f0544
mminzwoo4155 <minzwoo1234@gmail.com>
2022-01-23 21:43:53 +0700

    debug

--- app/Http/Controllers/UserRoleController.php
+++ app/Http/Controllers/UserRoleController.php
@@ -12,7 +12,6 @@ class UserRoleController extends Controller
         return response()->json(['Error' =>  "User ID does not exist!"],  400);
     }
     public function GetUserRoleFromIDFunctionWithoutRequest($userId){
-        return $userId;
         $users = UserRole::where('id','=', $userId)->get();
         if (count($users) > 0) return response()->json(["role"=> $users[0]->role],  200);
         return response()->json(['Error' =>  "User ID does not exist!"],  400);
